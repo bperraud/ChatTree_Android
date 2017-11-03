52505bae4680d80d792a1b100d315b849a49113a
Baptiste Perraud <baptiste.perraud@orange.fr>
2017-11-03 10:55:16 -0400

    Add some logos and view elements

--- app/src/main/java/com/chattree/chattree/LoginActivity.java
+++ app/src/main/java/com/chattree/chattree/LoginActivity.java
@@ -6,6 +6,7 @@ import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -62,10 +63,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View                 mProgressView;
     private View                 mLoginFormView;
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();

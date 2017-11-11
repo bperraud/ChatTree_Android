f8203c1753cf4022a1ec091c44ca32dbcdf99a95
Baptiste Perraud <baptiste.perraud@orange.fr>
2017-11-11 17:52:45 -0500

    Add toolbar

--- app/src/main/java/com/chattree/chattree/home/HomeActivity.java
+++ app/src/main/java/com/chattree/chattree/home/HomeActivity.java
@@ -1,26 +1,94 @@
 package com.chattree.chattree.home;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import com.chattree.chattree.R;
 
-import static com.chattree.chattree.login.LoginActivity.EXTRA_LOGIN_DATA;
-
 public class HomeActivity extends AppCompatActivity {
-
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Intent intent        = getIntent();
-        String loginDataJson = intent.getStringExtra(EXTRA_LOGIN_DATA);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        toolbar.setLogo(R.drawable.logo);
+        toolbar.setLogo(R.drawable.ic_more_vert_white_24dp);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        textView = (TextView) findViewById(R.id.textView);
-        textView.setText(loginDataJson);
+        ImageButton profileImageBtn = (ImageButton) findViewById(R.id.profileImageBtn);
+        profileImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("IMAGEBUTTON", "goto profile");
+            }
+        });
+
+//        Intent intent        = getIntent();
+//        String loginDataJson = intent.getStringExtra(EXTRA_LOGIN_DATA);
+//
+//        textView = (TextView) findViewById(R.id.textView);
+//        textView.setText(loginDataJson);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_toolbar_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                callSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+//                callSearch(newText);
+//                return true;
+                return false;
+            }
+
+            void callSearch(String query) {
+                // Do searching
+                Log.d("SEARCHING", "query: " + query);
+            }
+
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings1:
+                return true;
+
+            case R.id.action_settings2:
+                return true;
+
+            case R.id.action_notifications:
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }

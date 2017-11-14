b13c6e420dce72f295a3f0914087a06092ad69c0
Baptiste Perraud <baptiste.perraud@orange.fr>
2017-11-14 16:58:18 -0500

    Clean and review code

--- app/src/main/java/com/chattree/chattree/profile/ProfileActivity.java
+++ app/src/main/java/com/chattree/chattree/profile/ProfileActivity.java
@@ -1,15 +1,15 @@
-package com.chattree.chattree.profil;
+package com.chattree.chattree.profile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.chattree.chattree.R;
 
-public class ProfilActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profil);
+        setContentView(R.layout.activity_profile);
     }
 }

e69bb9eecd4fc3bccc7a77f0d6d2a167ec2136c6
Baptiste Perraud <baptiste.perraud@orange.fr>
2017-11-13 21:33:38 -0500

    Add FAB for new conversation

--- app/src/main/java/com/chattree/chattree/home/ConversationsListFragment.java
+++ app/src/main/java/com/chattree/chattree/home/ConversationsListFragment.java
@@ -1,10 +1,12 @@
 package com.chattree.chattree.home;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import com.chattree.chattree.R;
 
 public class ConversationsListFragment extends Fragment {
@@ -13,6 +15,15 @@ public class ConversationsListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout resource that'll be returned
         View rootView = inflater.inflate(R.layout.fragment_conversations_list, container, false);
+
+        // New conversation FAB
+        FloatingActionButton newConvFAB = rootView.findViewById(R.id.new_conv_fab);
+        newConvFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "NEW CONVERSATION", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
